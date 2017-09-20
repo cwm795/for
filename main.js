2f685a87443f940651dd58a7f9d3396adc22fd20
Chandler <cwm795@gmail.com>
2017-09-20 18:29:59 -0500

    added 2 for-loops that add and divide

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("Hi");
+
+
+
+
+   for(var i = 5; i < 120; i += 10) {
+	console.log("Current Value is", i);
+}
+
+for(var j = 4096; j  >= 1; j /= 2 ) {
+	console.log("Current Value is", j);
+}
